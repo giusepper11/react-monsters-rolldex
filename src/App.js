e671598bd172b35e330e9566243e8b182655016a
Giuseppe Rosa <giusepper11@gmail.com>
2019-08-20 17:00:49 -0300

    Adicionando o filtro e completando a aplicação

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component.jsx";
+import { SearchBox } from "./components/search-box/search-box.component.jsx";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     };
   }
 
@@ -17,10 +19,25 @@ class App extends Component {
       .then(users => this.setState({ monsters: users }));
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  };
+
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+
     return (
       <div className="App">
-        <CardList monsters={this.state.monsters} />
+      <h1>Monsters RollerDex</h1>
+        <SearchBox
+          placeholder="search monsters"
+          handleChange={this.handleChange}
+        />
+
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,11 +2,10 @@ import React from "react";
 import { Card } from "../card/card.component";
 import "./card-list.style.css";
 
-
 export const CardList = props => (
   <div className="card-list">
     {props.monsters.map(monster => (
-      <Card key={monster.id} monster={monster}/>
+      <Card key={monster.id} monster={monster} />
     ))}
   </div>
 );
