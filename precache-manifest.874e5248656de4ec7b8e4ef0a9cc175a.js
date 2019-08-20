93c73ef347552b8ae511ede8e9477d138825db2e
Giuseppe Rosa <giusepper11@gmail.com>
2019-08-20 17:04:24 -0300

    Updates

---
+++ precache-manifest.874e5248656de4ec7b8e4ef0a9cc175a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a75203ed20f1818a23a426ac732ab18",
+    "url": "/react-monsters-rolldex/index.html"
+  },
+  {
+    "revision": "44b6ecbc6036af68bc07",
+    "url": "/react-monsters-rolldex/static/css/main.9e0e3d75.chunk.css"
+  },
+  {
+    "revision": "d4cd09436fd033da2391",
+    "url": "/react-monsters-rolldex/static/js/2.3ab82e27.chunk.js"
+  },
+  {
+    "revision": "44b6ecbc6036af68bc07",
+    "url": "/react-monsters-rolldex/static/js/main.ac8d8c42.chunk.js"
+  },
+  {
+    "revision": "6cd3d0b4f651e79a870b",
+    "url": "/react-monsters-rolldex/static/js/runtime~main.9ce62fac.js"
+  }
+]);
